e79e70d67e3cef6c3ee255a630ebd1bbd3a71879
JulienJandot <julienjandot@orange.fr>
2018-03-06 09:35:44 +0100

    push

--- app/src/main/java/com/example/julien/smartown/MainActivity.java
+++ app/src/main/java/com/example/julien/smartown/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.julien.smartown;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.actionbar_layout);
+    }
+
+    public void launchActu(View view) {
+        Intent intent = new Intent(MainActivity.this, ActuActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchCommerce(View view) {
+        Intent intent = new Intent(MainActivity.this, CommercesActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchReseau(View view) {
+        Intent intent = new Intent(MainActivity.this, ReseauxActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchInterets(View view) {
+        Intent intent = new Intent(MainActivity.this, InteretsActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/julien/smartown/ReseauxActivity.java
@@ -0,0 +1,23 @@
+package com.example.julien.smartown;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Julien on 06/03/2018 for Smartown
+ */
+
+public class ReseauxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //TODO
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.actionbar_layout);
+    }
+}
